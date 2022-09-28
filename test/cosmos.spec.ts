760b3ee69298cc89255dc16cdbeb1a26d7e665c2
Bartosz Pieńkowski <pienkowb@gmail.com>
2022-09-28 18:36:12 +0200

    Format TypeScript files with Prettier

--- test/cosmos.spec.ts
+++ test/cosmos.spec.ts
@@ -1,13 +1,17 @@
 /// <reference types="@as-pect/assembly/types/as-pect" />
 
-import { Grant, encodeGrant, decodeGrant } from '../src/cosmos/authz/v1beta1/authz';
-import { Any } from '../src/google/protobuf/any';
-import { Timestamp } from '../src/google/protobuf/timestamp';
+import {
+  Grant,
+  encodeGrant,
+  decodeGrant,
+} from "../src/cosmos/authz/v1beta1/authz";
+import { Any } from "../src/google/protobuf/any";
+import { Timestamp } from "../src/google/protobuf/timestamp";
 
-describe('Cosmos SDK', () => {
-  test('decodes a grant', () => {
+describe("Cosmos SDK", () => {
+  test("decodes a grant", () => {
     const grant = new Grant(
-      new Any('/this.is.a.test', new Uint8Array(1024)),
+      new Any("/this.is.a.test", new Uint8Array(1024)),
       new Timestamp(123456, 654321)
     );
 
@@ -16,7 +20,7 @@ describe('Cosmos SDK', () => {
 
     const authorization = decodedGrant.authorization as Any;
 
-    expect(authorization.typeUrl).toBe('/this.is.a.test');
+    expect(authorization.typeUrl).toBe("/this.is.a.test");
     expect(authorization.value).toHaveLength(1024);
 
     const expiration = decodedGrant.expiration as Timestamp;
--- test/ibc.spec.ts
+++ test/ibc.spec.ts
@@ -1,33 +1,37 @@
 /// <reference types="@as-pect/assembly/types/as-pect" />
 
-import { MsgTransfer, encodeMsgTransfer, decodeMsgTransfer } from '../src/ibc/applications/transfer/v1/tx';
-import { Coin } from '../src/cosmos/base/v1beta1/coin';
+import {
+  MsgTransfer,
+  encodeMsgTransfer,
+  decodeMsgTransfer,
+} from "../src/ibc/applications/transfer/v1/tx";
+import { Coin } from "../src/cosmos/base/v1beta1/coin";
 
-describe('IBC', () => {
-  test('decodes a transfer message', () => {
+describe("IBC", () => {
+  test("decodes a transfer message", () => {
     const msg = new MsgTransfer(
-      'transfer',
-      'channel-141',
-      new Coin('atom', '100'),
-      'cosmos1hjct6q7npsspsg3dgvzk3sdf89spmlpfg8wwf7',
-      'osmo1hjct6q7npsspsg3dgvzk3sdf89spmlpfqua7lv',
+      "transfer",
+      "channel-141",
+      new Coin("atom", "100"),
+      "cosmos1hjct6q7npsspsg3dgvzk3sdf89spmlpfg8wwf7",
+      "osmo1hjct6q7npsspsg3dgvzk3sdf89spmlpfqua7lv",
       null,
-      67890,
+      67890
     );
 
     const encodedMsg = encodeMsgTransfer(msg);
     const decodedMsg = decodeMsgTransfer(encodedMsg);
 
-    expect(decodedMsg.sourcePort).toBe('transfer');
-    expect(decodedMsg.sourceChannel).toBe('channel-141');
+    expect(decodedMsg.sourcePort).toBe("transfer");
+    expect(decodedMsg.sourceChannel).toBe("channel-141");
 
     const token = decodedMsg.token as Coin;
 
-    expect(token.denom).toBe('atom');
-    expect(token.amount).toBe('100');
+    expect(token.denom).toBe("atom");
+    expect(token.amount).toBe("100");
 
-    expect(decodedMsg.sender).toBe('cosmos1hjct6q7npsspsg3dgvzk3sdf89spmlpfg8wwf7');
-    expect(decodedMsg.receiver).toBe('osmo1hjct6q7npsspsg3dgvzk3sdf89spmlpfqua7lv');
+    expect(decodedMsg.sender).toBe(msg.sender);
+    expect(decodedMsg.receiver).toBe(msg.receiver);
     expect(decodedMsg.timeoutHeight).toBeNull();
     expect(decodedMsg.timeoutTimestamp).toBe(67890);
   });
